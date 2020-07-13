c6c815740a7f4ec7bdf65770f628aef3521f8bf7
vikas kumar <viomjeet@gmail.com>
2020-07-13 09:53:00 +0530

    first commit

---
+++ build/precache-manifest.376e671e888f735c2ec2eb475a5adb93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2feab0a362bb7b40f6d5ac9bf79a7343",
+    "url": "/diagnal2/index.html"
+  },
+  {
+    "revision": "5cd3f02d1df7ad22aa4e",
+    "url": "/diagnal2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "47aaf4abecc3b54d6625",
+    "url": "/diagnal2/static/css/main.6ba2b295.chunk.css"
+  },
+  {
+    "revision": "5cd3f02d1df7ad22aa4e",
+    "url": "/diagnal2/static/js/2.8397fa31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/diagnal2/static/js/2.8397fa31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47aaf4abecc3b54d6625",
+    "url": "/diagnal2/static/js/main.76ca77ea.chunk.js"
+  },
+  {
+    "revision": "cf0ccb52c2848523b427",
+    "url": "/diagnal2/static/js/runtime-main.c5097423.js"
+  }
+]);
